6bc298e89ea7621e89e51d84aae2bfb699b14131
purnaa-l <mynameispurnaa@gmail.com>
2024-12-17 19:11:00 +0530

    feat: Light/Dark Mode integration.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTree } from '@fortawesome/free-solid-svg-icons';
+import { faTree, faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 
-function Navbar() {
+function Navbar({ theme, toggleTheme }) {
   return (
     <nav className="navbar navbar-expand-md" style={{ backgroundColor: '#000', padding: '1rem 0' }}>
       <div className="container-fluid">
-        {/* Logo Section with Tree Icon */}
+        {/* Logo Section */}
         <Link to="/" className="navbar-brand d-flex align-items-center" style={{ color: '#fff', fontFamily: 'Poiret One' }}>
           <FontAwesomeIcon icon={faTree} className="me-2" />
           AirSphere
@@ -26,11 +26,11 @@ function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        {/* Collapsible Navigation Links */}
+        {/* Navigation Links */}
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link to="/" className="nav-link" style={{ color: '#fff',  font:'Poiret One'}}>Home</Link>
+              <Link to="/" className="nav-link" style={{ color: '#fff' }}>Home</Link>
             </li>
             <li className="nav-item">
               <Link to="/fetch-aqi" className="nav-link" style={{ color: '#fff' }}>Fetch AQI</Link>
@@ -42,6 +42,14 @@ function Navbar() {
               <Link to="/view-past-aqis" className="nav-link" style={{ color: '#fff' }}>View Past AQIs</Link>
             </li>
           </ul>
+          {/* Theme Toggle Button */}
+          <button 
+            className="theme-toggle-button" 
+            onClick={toggleTheme} 
+            style={{ background: 'none', border: 'none', color: '#fff', fontSize: '1.5rem', marginLeft: '1rem', cursor: 'pointer' }}
+          >
+            {theme === 'light' ? <FontAwesomeIcon icon={faMoon} /> : <FontAwesomeIcon icon={faSun} />}
+          </button>
         </div>
       </div>
     </nav>
