c1d98603118c8d3c19fd785348f87242d4896d93
purnaa-l <mynameispurnaa@gmail.com>
2024-12-26 02:03:46 +0530

    feat: Refactored front-end

--- src/main/java/com/postgresql/aqi/service/AqiService.java
+++
@@ -1,9 +0,0 @@
-package com.postgresql.aqi.service;
-
-import com.postgresql.aqi.dto.AqiDto;
-import com.postgresql.aqi.entity.Aqi;
-
-public interface AqiService {
-    AqiDto createAqi(AqiDto aqiDto);
-    AqiDto getAqi(Long aqiId);
-}
